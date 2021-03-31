1c7bdc02639413aa51227897dcdbbe761a742716
Woranan Kamonlattana <eeywk2@nottingham.ac.uk>
2021-03-31 10:51:33 +0700

    edit travis

---
+++ worksheet8/maths.h
@@ -0,0 +1,17 @@
+// maths.h -----------------------------------
+
+#ifndef MATHS_H
+#define MATHS_H
+
+/** @file
+* This file contains the declarations of all exported functions.
+*/
+/** Brief description
+* This description will hopefully be picked up on by doxygen
+*/
+
+int add( int a, int b );
+
+#endif
+
+// /maths.h --------------------------------------
